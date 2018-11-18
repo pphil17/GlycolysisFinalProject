3259e36906145c87ad7ce828da27738737726158
wanjind <rndath1@lsu.edu>
2018-11-18 14:56:25 -0800

    Final

---
+++ Final.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+class cells:
+    def __init__(self,number,death,reproduction,generation):
+        #cell number
+        #threshhold for death of cells
+        #threshhold for reproduction(cell number of surviving cells double)
+        #if ATP 
+#class glycolytic pathway:
+    def __init__(self,Oxygencon,Glucosecons,protonprd,ATPprodc):
+        #equation for oxygen con
+        #equation for gluco con
+        #equation for proton prd
+        #equation for ATP prd
+#class non-glycolytic pathway
+    #def __init__(self,Oxygencon,Glucoseconc,prtonprd,atpprodcn)
+        #equation for Oxygencon
+        #equation for glucose
+        #equation for proton prd
+        #equation for ATP prd
--- README.md
+++
@@ -1,2 +0,0 @@
-# GlycolysisFinalProject
-This is the final project for Biol 4800 to demonstrate how glycolysis.
