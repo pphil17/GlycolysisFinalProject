79474c14d1ab4c0f7f627c2405526f2bdeb183f9
wanjind <rndath1@lsu.edu>
2018-11-20 13:12:31 -0800

    Fi

--- Final.py
+++ Final.py
@@ -1,19 +1,53 @@
 #!/usr/bin/env python
-class cells:
-    def __init__(self,number,death,reproduction,generation):
-        #cell number
-        #threshhold for death of cells
-        #threshhold for reproduction(cell number of surviving cells double)
-        #if ATP 
-#class glycolytic pathway:
-    def __init__(self,Oxygencon,Glucosecons,protonprd,ATPprodc):
+class glycolytic pathway:
+    def __init__(self,number,generations):
+        self.number=Cell number
+        self.generation= generations
+        Cell number=[10]
+        generations=[10]
+    def reproduction (self):
+        for gen in range(generations):
+            if fA=>0.8:
+                Cellnumber=Cellnumber[-1]*2
+            elif:
+                fA=>0.3
+                Cellnumber=cellnumber[-1]
+            elif:
+                fA=0.3
+                Cellnumber=0
+                Cellnumber.append(Cellnumber)
+    def printgraph (self):
+        
         #equation for oxygen con
+ Vo=0.012
+
+O=0.056
+
+Ko=0.05
+
+Fo=Vo*(O/(O+Ko))
+
+Fo
+Out[8]: 0.006339622641509434
         #equation for gluco con
+pG=1
+Ao=0.1
+G=5
+Kg=0.04
+Fg=-(((1*0.1/2)+(27*Fo/10))*(5/(5+0.04)))
         #equation for proton prd
+
         #equation for ATP prd
-#class non-glycolytic pathway
-    #def __init__(self,Oxygencon,Glucoseconc,prtonprd,atpprodcn)
+class non-glycolytic pathway
+def __init__(self,number,generations):
+        self.number=Cell number
+        self.generation= generations
+        Cell number=[10]
+        generations=[10]
+    def reproduction (self):
+
         #equation for Oxygencon
         #equation for glucose
         #equation for proton prd
         #equation for ATP prd
+    
