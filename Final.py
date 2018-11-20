6803d22db2a6caa852d992f4d381f96e3b690c2c
CompBio <Cameron F>
2018-11-20 12:51:25 -0800

    push

--- Final.py
+++ Final.py
@@ -1,19 +1,52 @@
 #!/usr/bin/env python
-class cells:
-    def __init__(self,number,death,reproduction,generation):
-        #cell number
+
+#user input to modify equation to begin tests
+
+#cells who do not live have increased hydrogen production equation for hydrogen
+	if fa >= 0.8:
+	return{ number*2 }
+#this is a rough draft
+	else
+	 fa <= 0.3:
+
+
+        kH = 0.00025
+        pG = pG
+        Vo =0.012
+        fo = fo 
+
+        return fH = kH * 5.8 * pG * ( Vo + fo )
+class cell:
+    def __init__(self, number, ATPfa, cellpH, generation):
+       	self.number = number
+	self.ATPfa = ATPfa
+	self.cellpH = cellpH
+	self.generation = generation
+	#number of cells
         #threshhold for death of cells
         #threshhold for reproduction(cell number of surviving cells double)
         #if ATP 
+Living = cell("GenOne", > = 0.8, 6.1, 2)
+Duplicating = cell("GenOne" 
 #class glycolytic pathway:
-    def __init__(self,Oxygencon,Glucosecons,protonprd,ATPprodc):
-        #equation for oxygen con
+class glycolytic:
+    def __init__(self,Oxygencon,Glucosecon,Protonprd,ATPprodc):
+	self.Oxygencon = Oxygencon
+	self.Glucosecon = Glucosecon
+	self.Protonprd = Protonprd
+	sel.ATPprodc = ATPprodc
+
+
+ #equation for oxygen con
         #equation for gluco con
         #equation for proton prd
         #equation for ATP prd
+
 #class non-glycolytic pathway
     #def __init__(self,Oxygencon,Glucoseconc,prtonprd,atpprodcn)
         #equation for Oxygencon
         #equation for glucose
         #equation for proton prd
         #equation for ATP prd
+
+
