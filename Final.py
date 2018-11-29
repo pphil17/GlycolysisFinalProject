6126d3233f6be86a1d9d84554c7cdba93737fa22
Peyton_Phillips <pphil17@lsu.edu>
2018-11-29 09:26:08 -0800

    Rough draft of pseudocode

--- Final.py
+++ Final.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+ #Cells use one of two methods to break down glucose and produce ATP, efficient aerobic pathway that requires oxygen or using glycolysis, an inefficient anaerobic pathway.
+ #Glycolysis is the metabolic process of breaking down carbohydrates, such as glucose, and making enzymes & ATP. Glycolysis is part of the anaerobic pathway and is used when oxygen levels are too low and cannot support the level of ATP being produced
+ #The model assumes that cells have a target level of ATP demand. The model is testing what happens to cells under anaerobic conditions (glycolysis), during ten geenrations, depending on the ATP levels produced.
 master
 class glycolytic pathway:
     def __init__(self,number,generations):
@@ -19,6 +23,10 @@ class glycolytic pathway:
                 Cellnumber.append(Cellnumber)
     def printgraph (self):
         
+#ATP production determines the behavior of the cell
+#ATP demand is higher for cancer cells than in a normal cell, so ATP production has to be higher in order for cell to reproduce
+#ATP provides cells with energy, but a certain amount of ATP has to be produced to allow cells to survive and reproduce, when ATP production is greater than 0.8, the cell has enough energy for reproduction, if ATP production is between 0.8 and 0.3, the cell doesn't have enough energy to reproduce,but it does have enough energy to survive. If ATP production is less than 0.3, the cell will not be able to survive and will die
+
         #equation for oxygen con
  Vo=0.012
 
@@ -30,7 +38,12 @@ Fo=Vo*(O/(O+Ko))
 
 Fo
 Out[8]: 0.006339622641509434
+
+#ATP levels are determined by glucose consumption and oxygen concentration
+#Oxygen consumption fO is determined by oxygen concentration (cells use glycolysis or areobic pathway)
+#since the cells are using glycolysis, oxygen con is low/near zero. Shows oxygen concentration is insufficient and cells will use glycolysis to produce ATP
   
+
 #user input to modify equation to begin tests
 
 #class glycolytic pathway:
@@ -42,10 +55,14 @@ class glycolytic:
 	sel.ATPprodc = ATPprodc
 
 
- #equation for oxygen con
+       #equation for oxygen con
+#Oxygen consumption determines oxygen concentration (which determines if cells use glycolysis or areobic pathway)
                    
  master
         #equation for gluco con
+#Cells uses glucose to produce ATP, but glycolysis is an insuffiecient way of producing ATP (2 ATP per glucose molecule broken down)
+#Glucose consumption is driven by the need to meet ATP demand.
+#pG is a multiplier representing the altered glucose metabolism seen in many tumor cells (i.e., the Warburg effect), G is the amount of glucose, Kg is the half-max concentration of glucose, pG
 import numpy as np
 
 pG = np.linspace(0, 1, 100)
@@ -56,7 +73,14 @@ Fg=-(((1*0.1/2)+(27*Fo/10))*(5/(5+0.04)))
         #equation for proton prd
 
         #equation for ATP prd
- master
+#For glycolytic pathway, Fo (oxygen con) should be low
+Fg=Fg
+Fo=Fo
+Fa=-(((2*Fg)+(27*Fo))/5)
+
+
+    #Model tests cell reproduction under aerobic (non-glycolytic) pathway for ten generations
+master
 class non-glycolytic pathway
 def __init__(self,number,generations):
         self.number=Cell number
@@ -73,6 +97,7 @@ def __init__(self,number,generations):
         #equation for Oxygencon
         #equation for glucose
         #equation for ATP prd
+#Proton production, Fh, is linked to the amount of glycolysis that does not feed the aerobic pathway
 #equation for proton prd
   if fa >= 0.8:
         return{ number*2 }
@@ -84,9 +109,13 @@ def __init__(self,number,generations):
         kH = 0.00025
         pG = pG
         Vo =0.012
-        fo = fo 
+        fo = fo
 
         return fH = kH * 5.8 * pG * ( Vo + fo )
+Fg=Fg
+fo=fo
+Fa=-(((2*Fg)+(27*fo))/5)
+
 class cell:
     def __init__(self, number, ATPfa, cellpH, generation):
         self.number = number
@@ -105,7 +134,7 @@ class cell:
         #number of cells
         #threshhold for death of cells
         #threshhold for reproduction(cell number of surviving cells double)
-        #if ATP 
+        #if ATP
 Living = cell("GenOne", > = 0.8, 6.1, 2)
 Duplicating = cell("GenOne" 
 
